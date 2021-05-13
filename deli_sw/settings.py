7652cfbceaafcdde1b9248ae1dee945ba4ae9c52
iainm342 <ism342@gmail.com>
2021-05-13 18:20:46 +0100

    complete emails

--- deli_sw/settings.py
+++ deli_sw/settings.py
@@ -178,3 +178,4 @@ STRIPE_CURRENCY = "gbp"
 STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY", "")
 STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY", "")
 STRIPE_WH_KEY = os.getenv("STRIPE_WH_KEY", "")
+DEFAULT_FROM_EMAIL = "delisw@example.com"
