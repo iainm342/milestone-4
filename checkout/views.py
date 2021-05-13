a88e763716e358f8c659855fcf99e40626bff7ee
iainm342 <ism342@gmail.com>
2021-05-13 12:18:34 +0100

    added strip elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = "checkout/checkout.html"
     context = {
         "order_form": order_form,
+        "stripe_public_key": "pk_test_51IaIRaFW2rFBZfbSkvjQmVopl7MOaKwBZLxG71LEE5PTekdN2c84ncWqFsUoG5SeQHpjCPi1VNBEaHWHvOl0OfKF00YZcF3Ekh",
+        "client_secret": "test_client_secret",
     }
 
     return render(request, template, context)
