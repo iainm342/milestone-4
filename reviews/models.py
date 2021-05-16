31d03a6566a6b2ec4b64eeb394f43862e9fb28cb
iainm342 <ism342@gmail.com>
2021-05-16 13:38:16 +0100

    added review model

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from profiles.models import UserProfile
+from products.models import Product
+
+
+class Review(models.Model):
+    """
+    Review model to allow user to leave a product review
+    """
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    rating = models.IntegerField(default=1)
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
