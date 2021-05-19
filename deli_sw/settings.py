eb3929ba36b4a74e3c1ca49b61f0a592632a0d6d
iainm342 <ism342@gmail.com>
2021-05-19 16:27:38 +0100

    changes to customs_storages and settings.py

--- deli_sw/settings.py
+++ deli_sw/settings.py
@@ -223,8 +223,9 @@ if'USE_AWS' in os.environ:
     MEDIAFILES_LOCATION = "media"
 
     # Override static and media URLs in production
-    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}"
-    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}"
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/"
+    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/"
+
 
 # Stripe
 
