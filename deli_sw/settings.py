a3ca3b5322fcef2d030a25462ee0bc2527cd5f56
iainm342 <ism342@gmail.com>
2021-05-22 12:50:16 +0100

    added allowed_hosts to settings.py

--- deli_sw/settings.py
+++ deli_sw/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['milestone-4-delisw.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -145,12 +145,17 @@ WSGI_APPLICATION = "deli_sw.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
