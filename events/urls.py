e33c4dbc75548f58c162d0e252efcb70dc584990
iainm342 <ism342@gmail.com>
2021-05-24 14:01:32 +0100

    added events link to navbar

--- events/urls.py
+++ events/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.post, name='post'),
+    path('', views.post, name='events'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
