b7803887c0a34f2ca31515729d312c8815a890f9
iainm342 <ism342@gmail.com>
2021-05-24 11:32:31 +0100

    added contact send email function

--- contact/views.py
+++ contact/views.py
@@ -1,4 +1,6 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render, redirect
+from django.core.mail import send_mail, BadHeaderError
 from django.contrib import messages
 
 from .forms import ContactForm
@@ -9,7 +11,14 @@ def contact(request):
     form = ContactForm(request.POST or None)
     if request.method == 'POST':
         if form.is_valid():
-
+            from_email = form.cleaned_data['from_email']
+            subject = form.cleaned_data['subject']
+            content = form.cleaned_data['content']
+            try:
+                send_mail(subject, content, from_email, [
+                    'iainm342dev@gmail.com'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found!')
             messages.success(request, 'Message sent!')
             return redirect('home')
 
